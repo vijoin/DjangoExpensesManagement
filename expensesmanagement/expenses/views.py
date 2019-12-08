d73fb09517a137915af4682911e6d799d9f3dbda
Victor Inojosa <vijoin@gmail.com>
2019-12-08 13:56:54 -0300

    Filter expenses by parameter passed via url

--- expensesmanagement/expenses/views.py
+++ expensesmanagement/expenses/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render
 
+from expenses.models import Expense
+
+
 def report_monthly_expenses(request, year, month):
-    data = {
-        'year': year,
-        'month': month,
-        }
-    return render(request, 'monthly_report.html', {'data': data})
+    data = {'year': year, 'month': month}
+    expenses = Expense.objects.filter(date__year=year, date__month=month)
+    return render(request, 'monthly_report.html', {'expenses': expenses, 'data': data})
